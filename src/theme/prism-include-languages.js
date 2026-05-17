e8d97383f0eace23825662675562d89061ec90af
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-17 13:00:34 -0400

    Add func keyword highlighting

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -20,7 +20,7 @@ export default function prismIncludeLanguages(Prism) {
       alias: "string",
     },
     keyword:
-      /\b(?:import|let|var|if|else|while|for|in|match|ok|err|return|struct|interface|on|pub|extern|inline|defer|type|sizeof|as|true|false|panic|break|continue)\b/,
+      /\b(?:import|let|var|if|else|while|for|in|match|ok|err|return|struct|interface|on|pub|extern|inline|defer|type|sizeof|as|true|false|panic|break|continue|func)\b/,
     builtin: {
       pattern:
         /\b(?:i8|i16|i32|i64|u8|u16|u32|u64|f32|f64|bool|str|char|rawptr|cstr|isize|usize)\b/,
