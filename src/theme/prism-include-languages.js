9bf78b9149dd65374a50ea9ba776ebf3bb039e9d
Ramon Asuncion <asuncionbatista@gmail.com>
2026-08-13 13:25:10 -0400

    Rewrite grammar

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -1,34 +1,75 @@
+const NEST_DEPTH = 4;
+
+function blockCommentPattern() {
+  const plain = '(?:[^*/]|\\*(?!\\/)|\\/(?!\\*))';
+  let pattern = '\\/\\*' + plain + '*\\*\\/';
+  for (let level = 0; level < NEST_DEPTH; level++) {
+    pattern = '\\/\\*(?:' + plain + '|' + pattern + ')*\\*\\/';
+  }
+  return new RegExp(pattern);
+}
+
 export default function prismIncludeLanguages(Prism) {
   Prism.languages.ripe = {
-    comment: {
-      pattern: /#.*/,
-    },
+    comment: [
+      {
+        pattern: blockCommentPattern(),
+        greedy: true,
+      },
+      {
+        pattern: /\/\/.*/,
+        greedy: true,
+      },
+    ],
     string: {
       pattern: /"(?:[^"\\]|\\.)*"/,
       greedy: true,
       inside: {
+        escape: {
+          pattern: /\\[nt\\"']|\\0|{{|}}/,
+          alias: 'constant',
+        },
         interpolation: {
-          pattern: /\{[^}]*\}/,
+          pattern: /{[^{}]*}/,
           inside: {
-            punctuation: /[{}]/,
+            punctuation: /^{|}$/,
           },
         },
       },
     },
-    "char-literal": {
+    'char-literal': {
       pattern: /'(?:[^'\\]|\\.)'/,
-      alias: "string",
+      greedy: true,
+      alias: 'string',
+    },
+    'function-definition': {
+      pattern: /\b(func)\s+[A-Za-z_]\w*/,
+      inside: {
+        keyword: /^func/,
+        function: /\w+$/,
+      },
+    },
+    'import-path': {
+      pattern: /\b(import|module)\s+[A-Za-z_]\w*(?:\.[A-Za-z_]\w*)*/,
+      inside: {
+        keyword: /^\w+/,
+        'class-name': /[\w.]+$/,
+      },
     },
     keyword:
-      /\b(?:import|let|var|if|else|while|for|in|match|ok|err|return|struct|interface|on|pub|extern|inline|defer|type|sizeof|as|true|false|panic|break|continue|func)\b/,
+      /\b(?:var|func|struct|enum|type|newtype|comptime|module|import|if|else|while|for|in|break|continue|return|loop|match|extern|pub|as|sizeof)\b/,
     builtin: {
       pattern:
-        /\b(?:i8|i16|i32|i64|u8|u16|u32|u64|f32|f64|bool|str|char|rawptr|cstr|isize|usize)\b/,
-      alias: "class-name",
+        /\b(?:i8|i16|i32|i64|u8|u16|u32|u64|isize|usize|f32|f64|bool|cstr|char|str|int|float|never|opaque)\b/,
+      alias: 'class-name',
     },
-    function: /\b[a-zA-Z_][a-zA-Z0-9_]*(?=\s*[\[({])/,
-    number: /\b(?:0x[\da-fA-F]+|\d+(?:\.\d+)?(?:[eE][+-]?\d+)?)\b/,
-    operator: /\.\.\.?|->|=>|[+\-*/%&|^~<>]=?|[!=]=|[@^?!]/,
-    punctuation: /[{}[\]();,.]/,
+    boolean: /\b(?:true|false)\b/,
+    constant: /\b(?:null|undefined)\b/,
+    function: /\b[A-Za-z_]\w*(?=\s*\()/,
+    number:
+      /\b(?:0[xX][\da-fA-F][\da-fA-F_]*|0[bB][01][01_]*|0[oO][0-7][0-7_]*|\d[\d_]*(?:\.\d[\d_]*)?(?:[eE][+-]?\d[\d_]*)?)(?:i8|i16|i32|i64|isize|u8|u16|u32|u64|usize|f32|f64)?\b/,
+    operator:
+      /\.\.=|\.\.\.|\.\.|<<=?|>>=?|&&|\|\||[+\-*/%&|^]=?|[=!<>]=|[=!~<>]/,
+    punctuation: /[{}[\]();,.:]/,
   };
 }
