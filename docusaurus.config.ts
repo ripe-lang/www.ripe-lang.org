4b9dc517bc2a12f8875fd19c860c2c3d1b755fda
Ramon Asuncion <asuncionbatista@gmail.com>
2026-08-13 13:25:03 -0400

    Update nav and footer links for new docs pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,128 +1,131 @@
-import { themes as prismThemes } from "prism-react-renderer";
-import { ripeDark, ripeLight } from "./src/ripe-theme";
-import type { Config } from "@docusaurus/types";
-import type * as Preset from "@docusaurus/preset-classic";
+import { themes as prismThemes } from 'prism-react-renderer';
+import { ripeDark, ripeLight } from './src/ripe-theme';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: "Ripe",
-  tagline: "A systems programming language",
-  favicon: "img/logo.png",
+  title: 'Ripe',
+  tagline: 'A systems programming language',
+  favicon: 'img/logo.png',
 
   future: {
     v4: true,
   },
 
-  url: "https://www.ripe-lang.org",
-  baseUrl: "/",
+  url: 'https://www.ripe-lang.org',
+  baseUrl: '/',
 
-  organizationName: "ripe-lang",
-  projectName: "www.ripe-lang.org",
+  organizationName: 'ripe-lang',
+  projectName: 'www.ripe-lang.org',
   trailingSlash: false,
 
-  onBrokenLinks: "warn",
+  onBrokenLinks: 'warn',
 
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       {
         docs: {
-          sidebarPath: "./sidebars.ts",
-          editUrl: "https://github.com/ripe-lang/www.ripe-lang.org/edit/main/",
+          sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/ripe-lang/www.ripe-lang.org/edit/main/',
         },
         blog: {
           showReadingTime: true,
-          feedOptions: { type: ["rss", "atom"], xslt: true },
-          editUrl: "https://github.com/ripe-lang/www.ripe-lang.org/edit/main/",
-          onInlineTags: "warn",
-          onInlineAuthors: "warn",
-          onUntruncatedBlogPosts: "warn",
+          feedOptions: { type: ['rss', 'atom'], xslt: true },
+          editUrl: 'https://github.com/ripe-lang/www.ripe-lang.org/edit/main/',
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
         },
         theme: {
-          customCss: "./src/css/custom.css",
+          customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
-    image: "img/combination_dark.png",
+    image: 'img/combination_dark.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: "Ripe",
+      title: 'Ripe',
       logo: {
-        alt: "Ripe",
-        src: "img/logo.png",
-        srcDark: "img/logo.png",
-        href: "/",
+        alt: 'Ripe',
+        src: 'img/logo.png',
+        srcDark: 'img/logo.png',
+        href: '/',
       },
       items: [
         {
-          type: "docSidebar",
-          sidebarId: "docsSidebar",
-          position: "right",
-          label: "Docs",
+          type: 'docSidebar',
+          sidebarId: 'docsSidebar',
+          position: 'right',
+          label: 'Docs',
         },
         {
-          to: "/docs/reference/types",
-          label: "Packages",
-          position: "right",
+          to: '/docs/reference',
+          label: 'Reference',
+          position: 'right',
         },
         {
-          to: "/docs/getting-started/installation",
-          label: "Download",
-          position: "right",
+          to: '/docs/faq',
+          label: 'FAQ',
+          position: 'right',
         },
         {
-          to: "/blog",
-          label: "Blog",
-          position: "right",
+          to: '/docs/install',
+          label: 'Install',
+          position: 'right',
         },
         {
-          href: "https://github.com/ripe-lang/ripe",
-          label: "GitHub",
-          position: "right",
+          to: '/blog',
+          label: 'Blog',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/ripe-lang/ripe',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
       links: [
         {
-          title: "Learn",
+          title: 'Learn',
           items: [
-            {
-              label: "Getting Started",
-              to: "/docs/getting-started/installation",
-            },
-            { label: "Language Tour", to: "/docs/tutorial/variables" },
-            { label: "Reference", to: "/docs/reference/types" },
+            { label: 'Introduction', to: '/docs/intro' },
+            { label: 'Install', to: '/docs/install' },
+            { label: 'Reference', to: '/docs/reference' },
+            { label: 'FAQ', to: '/docs/faq' },
           ],
         },
         {
-          title: "Community",
+          title: 'Community',
           items: [
-            { label: "GitHub", href: "https://github.com/ripe-lang/ripe" },
+            { label: 'GitHub', href: 'https://github.com/ripe-lang/ripe' },
             {
-              label: "Issues",
-              href: "https://github.com/ripe-lang/ripe/issues",
+              label: 'Issues',
+              href: 'https://github.com/ripe-lang/ripe/issues',
             },
             {
-              label: "Contributing",
-              href: "https://github.com/ripe-lang/ripe/blob/main/CONTRIBUTING.md",
+              label: 'Contributing',
+              href: 'https://github.com/ripe-lang/ripe/blob/main/CONTRIBUTING.md',
             },
           ],
         },
         {
-          title: "More",
+          title: 'More',
           items: [
-            { label: "Blog", to: "/blog" },
-            { label: "Source", href: "https://github.com/ripe-lang/ripe" },
+            { label: 'Blog', to: '/blog' },
+            { label: 'Source', href: 'https://github.com/ripe-lang/ripe' },
           ],
         },
       ],
